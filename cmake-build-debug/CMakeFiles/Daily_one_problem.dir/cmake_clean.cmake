9d887cde77d66a7faba0bd7ca8d2ba5a0dd8603f
Lixiao <1240847431@qq.com>
2021-09-23 18:29:34 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/Daily_one_problem.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Daily_one_problem.dir/main.cpp.o"
+  "Daily_one_problem"
+  "Daily_one_problem.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Daily_one_problem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
