83658bb461a671c945a93f1a4d269215f68dadb4
Lixiao <1240847431@qq.com>
2021-09-23 19:16:23 +0800

    326：3的幂～LeetCode已打卡； cout输出是0/1，而不是bool值，这点有问题；

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
+using namespace std;
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        while (n && n % 3 == 0) {
+            n = n / 3;
+        }
+        return n == 1;
+    }
+};
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    Solution solution;
+    cout << solution.isPowerOfThree(3);
 }
