2326cbbd8aa5da487beba2e7475aa21fd6310046
Lixiao <1240847431@qq.com>
2021-10-10 09:13:29 +0800

    git add . 非重要文件

--- cmake-build-debug/CMakeFiles/Daily_one_problem.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/Daily_one_problem.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/lx/CLionProjects/Daily-one-problem/PAT_B/B1018.cpp" "/Users/lx/CLionProjects/Daily-one-problem/cmake-build-debug/CMakeFiles/Daily_one_problem.dir/PAT_B/B1018.cpp.o"
+  "/Users/lx/CLionProjects/Daily-one-problem/PAT_B/B1019.cpp" "/Users/lx/CLionProjects/Daily-one-problem/cmake-build-debug/CMakeFiles/Daily_one_problem.dir/PAT_B/B1019.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
