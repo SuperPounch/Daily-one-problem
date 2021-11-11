16202c9d560f1e0ffe01fe3a72f78a947b59fd57
Lixiao <1240847431@qq.com>
2021-11-11 09:02:34 +0800

    finish B1033

--- PAT_B/B1033.cpp
+++ PAT_B/B1033.cpp
@@ -10,7 +10,9 @@ int main(){
     getline(cin,bad);
     getline(cin,should);
     for (int i = 0,length=should.length(); i <length ; ++i) {
-        if (bad.find(toupper(should[i]))!=string::npos)continue;
-
+        if (bad.find(toupper(should[i]))!=string::npos)continue;//字母在坏键里
+        if (isupper(should[i])&&bad.find("+")!=string::npos)continue;//无法大写
+        cout<<should[i];
     }
+    return 0;
 }
