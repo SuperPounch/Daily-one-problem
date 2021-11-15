7da6e6d0fb2511a46a466aba34c001ffc01100a1
Lixiao <1240847431@qq.com>
2021-11-15 20:42:21 +0800

    upload B1033

--- PAT_B/B1033.cpp
+++ PAT_B/B1033.cpp
@@ -5,14 +5,14 @@
 
 using namespace std;
 
-int main(){
-    string bad,should;
-    getline(cin,bad);
-    getline(cin,should);
-    for (int i = 0,length=should.length(); i <length ; ++i) {
-        if (bad.find(toupper(should[i]))!=string::npos)continue;//字母在坏键里
-        if (isupper(should[i])&&bad.find("+")!=string::npos)continue;//无法大写
-        cout<<should[i];
+int main() {
+    string bad, should;
+    getline(cin, bad);
+    getline(cin, should);
+    for (int i = 0, length = should.length(); i < length; ++i) {
+        if (bad.find(toupper(should[i])) != string::npos)continue;//字母在坏键里
+        if (isupper(should[i]) && bad.find("+") != string::npos)continue;//无法大写
+        cout << should[i];
     }
     return 0;
 }
