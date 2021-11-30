afd168bc00ae4a048c76e817274642dc87d5ddab
Lixiao <1240847431@qq.com>
2021-11-30 18:26:44 +0800

    有理数分数的四则运算

--- PAT_B/B1034.cpp
+++ PAT_B/B1034.cpp
@@ -2,9 +2,44 @@
 // Created by 李啸 on 11/11/21.
 //
 #include "iostream"
-
+#include <cmath>
 using namespace std;
+long long a, b, c, d;
+long long gcd(long long t1, long long t2) {
+    //负责化简，得到可同除的最大项
 
+    return t2 == 0 ? t1 : gcd(t2, t1 % t2);
+    //t2==0,cout t1  t2!=0,gcd(t2,t1%t2)
+}
+void func(long long m, long long n) {
+    //分母为0 ，则输出inf
+    if (m * n == 0) {
+        printf("%s", n == 0 ? "Inf" : "0");
+        return ;
+    }
+    //异号处理
+    bool flag = ((m < 0 && n > 0) || (m > 0 && n < 0));
+    m = abs(m); n = abs(n);
+    //得到整数
+    long long x = m / n;
+    printf("%s", flag ? "(-" : "");//负号
+    if (x != 0) printf("%lld", x);
+
+    if (m % n == 0) {
+        if(flag) printf(")");
+        return ;
+    }
+    if (x != 0) printf(" ");
+    m = m - x * n;
+    long long t = gcd(m, n);
+    m = m / t; n = n / t;
+    printf("%lld/%lld%s", m, n, flag ? ")" : "");
+}
 int main() {
-    double
+    scanf("%lld/%lld %lld/%lld", &a, &b, &c, &d);
+    func(a, b); printf(" + "); func(c, d); printf(" = "); func(a * d + b * c, b * d); printf("\n");
+    func(a, b); printf(" - "); func(c, d); printf(" = "); func(a * d - b * c, b * d); printf("\n");
+    func(a, b); printf(" * "); func(c, d); printf(" = "); func(a * c, b * d); printf("\n");
+    func(a, b); printf(" / "); func(c, d); printf(" = "); func(a * d, b * c);
+    return 0;
 }
