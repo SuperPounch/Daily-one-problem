84e74c57f131ef882775ba4715d7673f51057ef1
Lixiao <1240847431@qq.com>
2022-02-24 23:11:22 +0800

    over 2.23 Easy917

--- LeetCode/Easy917.cpp
+++ LeetCode/Easy917.cpp
@@ -8,4 +8,31 @@
 //left从左往右扫描，right从右往左扫描
 //交换条件：两指针都扫描到字母，且left<right，交换s[left] and s[right]
 
+#include "iostream"
 
+using namespace std;
+
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int n = s.size();
+        int left = 0, right = n - 1;
+        while (true) {
+            while (left < right && !isalpha(s[left])) { // 判断左边是否扫描到字母，直到扫描到字母位置
+                left++;
+            }
+            while (right > left && !isalpha(s[right])) { // 判断右边是否扫描到字母，直到扫描到字母位置
+                right--;
+            }
+            if (left >= right) {  // 此时两指针交错，表示交换结束
+                break;
+            }
+            swap(s[left], s[right]);
+            left++;
+            right--;
+        }//一次while循环完成一次交换
+        return s;
+    }
+};
+
+//LeetCode官解
