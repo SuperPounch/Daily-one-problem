dc77a36827f2f34a7df16ad0af82536acad63e87
Lixiao <1240847431@qq.com>
2022-03-29 20:07:12 +0800

    Easy693,交替位二进制,位运算优化利用性质

--- LeetCode/Easy/Easy693.cpp
+++ LeetCode/Easy/Easy693.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-//方法：模拟
+//Method1：模拟
 /*class Solution {
 public:
     bool hasAlternatingBits(int n) {
@@ -21,6 +21,9 @@ public:
         return true;
     }
 };*/
+
+// Method2：位运算
+/*
 class Solution {
 public:
     bool hasAlternatingBits(int n) {
@@ -34,6 +37,22 @@ public:
         return true;//否则,true
     }
 };
+*/
+
+/*Method：更简单的位运算，利用交替位二进制数的性质
+性质：当给定值 nn 为交替位二进制数时，将 nn 右移一位得到的值 mm 仍为交替位二进制数，且与原数 nn 错开一位，两者异或能够得到形如 0000...11110000...1111 的结果 xx，此时对 xx 执行加法（进位操作）能够得到形如 0000...100000000...10000 的结果，将该结果与 xx 执行按位与后能够得到全 00 结果。*/
+class Solution {
+public:
+     bool hasAlternatingBits(int n) {
+         long long int x = n ^ (n >> 1);//int 通过不了
+        return (x & (x + 1)) == 0;
+    }
+};
+//时间和空间复杂度均O(1)
+/*作者：AC_OIer
+链接：https://leetcode-cn.com/problems/binary-number-with-alternating-bits/solution/gong-si-shui-by-ac_oier-zuw7/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。*/
 
 int main() {
     Solution solution;
