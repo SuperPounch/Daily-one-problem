f0f3d33fa54507420b7b74e966e25bedbf7f5351
Lixiao <1240847431@qq.com>
2022-10-13 15:34:11 +0800

    staged changes

--- LeetCode/Easy/Easy350.cpp
+++ LeetCode/Easy/Easy350.cpp
@@ -1,4 +1,23 @@
 //
 // Created by 李啸 on 2022/10/2.
 //
-
+#include "vector"
+#include "unordered_map"
+using namespace std;
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int>res;//result
+        unordered_map<int,int> map;//record num--count
+        if(nums1.size()>nums2.size())return intersect(nums2,nums1);//遍历小数组，减少遍历内存
+        //遍历
+        for(int num:nums1){
+            map[num]++;
+        }
+        for(int num:nums2){
+            if(map[num]>0){res.push_back(num);}
+            map[num]--;
+        }
+        return res;
+    }
+};
--- LeetCode/Medium/Medium36.cpp
+++ LeetCode/Medium/Medium36.cpp
@@ -1,4 +1,30 @@
 //
 // Created by 李啸 on 2022/10/6.
-//
+//有效的数独：判断已有的数字是否符合数独的规则
+#include "iostream"
+#include "vector"
+
+using namespace std;
+
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>> &board) {
+        //hasp<<int[][]<<">>j"
+        int row[9][10] = {0}, col[9][10] = {0}, block[9][10] = {0};
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.')continue;
+                int num = board[i][j] - '0';
+                int idx = j / 3 + i / 3 * 3;
+                if (row[i][num] || col[num][j] || block[idx][num])return false;
+                else {
+                    row[i][num] = 1;
+                    col[num][j] = 1;
+                    block[idx][num] = 1;
+                }
+            }
+        }
+        return true;
+    }
+};
 
